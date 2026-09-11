431c11cb0b89b751a80c0b5a3b2965d521b609fe
chirs <chrisedgemon@gmail.com>
2026-09-10 23:15:19 -0500

    Add irregular teams: Northern Ireland League All-Stars, Mexico XI, Nigeria XI

---
+++ data/teams/irregular.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import datetime
+
+l = [
+
+    { 'name': 'Northern Ireland League All-Stars' },
+    { 'name': 'Mexico XI' },
+    { 'name': 'Nigeria XI' },
+
+]
